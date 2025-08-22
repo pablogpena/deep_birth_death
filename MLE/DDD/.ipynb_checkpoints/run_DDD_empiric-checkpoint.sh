c8d3f2f0cc70f680023f6ca9cb10619951b7b523
pablogpena <7pablo98@gmail.com>
2025-08-22 16:01:50 +0000

    Added final results for MLE empirical inferece, and the MD archive

--- MLE/DDD/.ipynb_checkpoints/run_DDD_empiric-checkpoint.sh
+++ MLE/DDD/.ipynb_checkpoints/run_DDD_empiric-checkpoint.sh
@@ -1,1 +1,5 @@
-{ time Rscript run_DDD_empiric.r; } 2> /workspace/deep_birth_death/MLE/inference_data/time/empiric_DDD_time.txt
+# +
+#{ time Rscript run_DDD_empiric.r; } 2> /workspace/deep_birth_death/MLE/inference_data/time/empiric_DDD_time.txt
+# -
+
+{ time Rscript run_DDD_empiric.r; }
